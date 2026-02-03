a13c4bc5a30e3108b3cbedcd92a2371373e43e57
alubinski <adrian.lubinski.dev@proton.me>
2026-02-03 15:25:24 +0100

    Add ignore rules to telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,48 @@
 local config = function()
-  local telescope = require('telescope')
-  telescope.setup({
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-j>"] = "move_selection_next",
-          ["<C-k>"] = "move_selection_previous",
-        },
-      },
-    },
-    pickers = {
-      find_files = {
-        theme = "dropdown",
-        previewer = true,
-        hidden = true,
-      },
-      live_grep = {
-        theme = "dropdown",
-        previewer = true,
-      },
-      find_buffers = {
-        theme = "dropdown",
-        previewer = true,
-      },
-      colorscheme = {
-        enable_preview = true,
-      }
-    },
-  })
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			file_ignore_patterns = {
+				"build/",
+				"out/",
+				"node_modules/",
+				".git/",
+				"%.o", -- Ignore object files
+				"%.a", -- Ignore static libraries
+				"%.out", -- Ignore executables
+			},
+			mappings = {
+				i = {
+					["<C-j>"] = "move_selection_next",
+					["<C-k>"] = "move_selection_previous",
+				},
+			},
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown",
+				previewer = true,
+				hidden = true,
+			},
+			live_grep = {
+				theme = "dropdown",
+				previewer = true,
+			},
+			find_buffers = {
+				theme = "dropdown",
+				previewer = true,
+			},
+			colorscheme = {
+				enable_preview = true,
+			},
+		},
+	})
 end
 
-
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim'},
-    lazy = true,
-    config = config,
-    keys = {
-    },
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = true,
+	config = config,
+	keys = {},
 }
